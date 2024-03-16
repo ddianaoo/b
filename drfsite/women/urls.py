9806de41dd369cdb4790202bc7bf40fd3ca73f77
ddianaoo <dianauzun2006@gmail.com>
2024-03-16 18:25:48 +0200

    APIView example

--- drfsite/women/urls.py
+++ drfsite/women/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
-from .views import CategoryViewSet, WomenViewSet
+from .views import CategoryViewSet, WomenList, WomenDetail
 from rest_framework import routers
 
 r = routers.DefaultRouter()
 r.register(r'category', CategoryViewSet)
-r.register(r'women', WomenViewSet)
 app_name = 'women'
 
 urlpatterns = [
     path('', include(r.urls)),
+    path('women/', WomenList.as_view()),
+    path('women/<pk>/', WomenDetail.as_view()),
 ]
