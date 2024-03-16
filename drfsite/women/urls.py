e47508f8c9d03093a4f8ce911c868adefb59a5cd
ddianaoo <dianauzun2006@gmail.com>
2024-03-16 18:52:08 +0200

    ViewSet example

--- drfsite/women/urls.py
+++ drfsite/women/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
-from .views import CategoryViewSet, WomenList, WomenDetail
+from .views import CategoryViewSet, WomenViewSet
 from rest_framework import routers
 
 r = routers.DefaultRouter()
 r.register(r'category', CategoryViewSet)
+r.register(r'women', WomenViewSet, basename='women')
 app_name = 'women'
 
 urlpatterns = [
     path('', include(r.urls)),
-    path('women/', WomenList.as_view()),
-    path('women/<pk>/', WomenDetail.as_view()),
 ]
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Women, Category
@@ -7,44 +8,37 @@ from .serializers import WomenSerializer, CategorySerializer
 from django.http import Http404
 
 
-class WomenList(APIView):
+class WomenViewSet(ViewSet):
 
-    def get(self, request):
-        objects = Women.objects.all()
-        serializer = WomenSerializer(objects, many=True)
+    def get_queryset(self):
+        return Women.objects.all()
+
+    def list(self, request):
+        serializer = WomenSerializer(self.get_queryset(), many=True)
         return Response(serializer.data)
 
-    def post(self, request):
+    def retrieve(self, request, pk=None):
+        item = get_object_or_404(self.get_queryset(), pk=pk)
+        serializer = WomenSerializer(item)
+        return Response(serializer.data)
+
+    def create(self, request):
         serializer = WomenSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-class WomenDetail(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Women.objects.get(pk=pk)
-        except:
-            raise Http404
-
-    def get(self, request, pk):
-        item = self.get_object(pk=pk)
-        serializer = WomenSerializer(item)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
     def put(self, request, pk):
-        item = self.get_object(pk)
+        item = get_object_or_404(self.get_queryset(), pk=pk)
         serializer = WomenSerializer(item, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        item = self.get_object(pk=pk)
+    def destroy(self, request, pk):
+        item = get_object_or_404(self.get_queryset(), pk=pk)
         item.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
